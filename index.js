7542f3f1059256885683db438fe19f8eba3b9a83
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2017-01-14 01:32:41 +0900

    Add color

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 'use strict';
+
+const foregroundColor = '#eff0eb';
+const backgroundColor = '#282a36';
+const red = '#ff5c57';
+const green = '#5af78e';
+const yellow = '#f3f99d';
+const blue = '#57c7ff';
+const magenta = '#ff6ac1';
+const cyan = '#9aedfe';
+const colors = {
+  black: backgroundColor,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white: '#f1f1f0',
+  lightBlack: '#686868',
+  lightRed: red,
+  lightGreen: green,
+  lightYellow: yellow,
+  lightBlue: blue,
+  lightMagenta: magenta,
+  lightCyan: cyan,
+  lightWhite: foregroundColor
+};
+
 exports.decorateConfig = config => Object.assign({}, config, {
+  backgroundColor,
+  foregroundColor,
+  borderColor: '#222430',
+  cursorColor: '#97979b',
+  colors: colors,
   css: `
     ${config.css}
     .hyperterm_main { border: none; }
